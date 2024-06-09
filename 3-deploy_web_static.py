0518627f6202e3d2683db4d61acfcf8c097cc607
Daniel-IRYIVUZE <d.iryivuze@alustudent.com>
2024-06-09 18:15:17 +0000

    Made by Daniel

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists
 
-env.hosts = ['54.242.133.62', '34.233.128.212']
+env.hosts = ['3.85.234.100', '3.94.111.188']
 env.user = "ubuntu"
 env.key = "~/.ssh/id_rsa"
 
